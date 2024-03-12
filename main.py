891bee9f775dd75a2a907f4d8c427cdfc5fd1471
MapleTwig <conollyallen2@gmail.com>
2024-03-11 22:01:18 -0700

    Added Oil ETL Script and dataset

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from data_warehouse.connection import DW_Interface
 from scripts.ETL.company_table import Company_ETL
+from scripts.ETL.oil_table import Oil_ETL
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -13,3 +14,4 @@ wallet_password = os.getenv('WALLET_PASSWORD')
 
 DW_Interface = DW_Interface(config_dir, user, password, dsn, wallet_location, wallet_password)
 Company_ETL = Company_ETL(DW_Interface)
+Oil_ETL = Oil_ETL(DW_Interface)

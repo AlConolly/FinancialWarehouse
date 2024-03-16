b4757f91b20c2ae1a59492c80d01c8b40b613ea9
Callum Brezden <callumbrezden@gmail.com>
2024-03-16 13:33:49 -0700

    Setup unit test for get_date_time

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from oracle.connection import DW_Interface
 
 # ETL Utils
 from scripts.ETL.utils.script_time_tracker import ScriptTimeTracker
-from scripts.ETL.utils.daily_transactions import DailyTransactions
+from scripts.ETL.utils.daily_transactions import Daily_Transactions_ETL
 
 # ETL Classes
 from scripts.ETL.currency.currency import Currency_ETL
@@ -67,7 +67,7 @@ def run_script():
         'Enter the number of the ETL script you want to run', type=int) - 1
     script = scripts[choice]
     script_class = script_classes[script]
-    script_class(DW_Interface, DailyTransactions, Script_Tracker)
+    script_class(DW_Interface, Daily_Transactions_ETL, Script_Tracker)
 
 if __name__ == '__main__':
     run_script()
